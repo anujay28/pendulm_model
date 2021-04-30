77cd8af1d1d057bdd7a28966a414f413e5e97d14
nathaniel <nagoldfarb@wpi.edu>
2021-04-30 15:51:28 -0400

    working on the SMC

--- connect_pend_runner.m
+++ connect_pend_runner.m
@@ -2,21 +2,30 @@
 m1 = 1.0;
 l1 = 1.0;
 m2 = 1.0 + m1;
-l2 = = l1;
-
+l2 = l1;
+g = 9.81;
 
 % connection dyamics
-spring = 100;
-dampener = 100;
+spring = 10;
+dampener = 1;
 
 
 % model 1 gains
 Km1 = 30;
 Dm1 = 30;
 
+% model 2 gains
+Km2 = 20;
+Dm2 = 20;
 
 % Addmittance Controller
-Kadd_0 = 50;
-Badd_0 = 50;
+Kd = 50;
+Bd = 50;
+Md = 100;
 alpha = 10;
 beta = 10;
+
+
+% SMC
+lambda = 15;
+rho = 15;
