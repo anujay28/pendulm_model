b7d1c8ad6dbcbae379b73e70474483057740af94
nathaniel <nagoldfarb@wpi.edu>
2021-04-30 19:33:19 -0400

    SMC seems to be working

--- connect_pend_runner.m
+++ connect_pend_runner.m
@@ -3,11 +3,11 @@ m1 = 1.0;
 l1 = 1.0;
 m2 = 1.0 + m1;
 l2 = l1;
-g = 9.81;
+g = -9.81;
 
 % connection dyamics
-spring = 10;
-dampener = 1;
+spring = 300;
+dampener = 300;
 
 
 % model 1 gains
@@ -19,13 +19,13 @@ Km2 = 20;
 Dm2 = 20;
 
 % Addmittance Controller
-Kd = 50;
-Bd = 50;
-Md = 100;
+Kd = 10;
+Bd = 10;
+Md = 10;
 alpha = 10;
 beta = 10;
 
 
 % SMC
-lambda = 15;
-rho = 15;
+lambda = .005;
+rho = 0.0150;
