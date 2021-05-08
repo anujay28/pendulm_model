b106fe0f2ebf3bfaebb2d962784b36fade4417fc
nathaniel <nagoldfarb@wpi.edu>
2021-05-08 16:09:10 -0400

    working on kalman

---
+++ double_pend/myMeasurementFcn.m
@@ -0,0 +1,7 @@
+function x = myMeasurementFcn(q)
+%MYMEASUREMENTFCN Summary of this function goes here
+%   Detailed explanation goes here
+x = q;
+
+end
+
---
+++ double_pend/myStateTransitionFcn.m
@@ -0,0 +1,19 @@
+function x = myStateTransitionFcn(x ,u)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+   
+q = x(1:2);
+qd = x(3:4);
+
+m = [2, 1]; 
+
+
+% other model params
+l = [ 1.0, 1.5];
+
+temp = pinv(M(m, l, q))*(u - C(m, l, q, qd) - G(m, q));
+
+x= [qd(1); temp(1); qd(2); temp(2)]; 
+
+end
+
