5477e4ebf08235f6cb1d4fd218f4a5c8e8acf5e9
nathaniel <nagoldfarb@wpi.edu>
2021-05-09 00:01:53 -0400

    still trying things

--- double_pend/myStateTransitionFcn.m
+++ double_pend/myStateTransitionFcn.m
@@ -11,7 +11,7 @@ m = [2, 1];
 % other model params
 l = [ 1.0, 1.5];
 
-temp = pinv(M(m, l, q))*(u - C(m, l, q, qd) - G(m, q));
+temp = pinv(M(m, l, q))*(u - C(m, l, q, qd) - G(m, q) - [100 0; 0 100]*qd.^2);
 
 x= [qd(1); temp(1); qd(2); temp(2)]; 
 
