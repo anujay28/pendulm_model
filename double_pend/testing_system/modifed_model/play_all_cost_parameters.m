940be03209ae870571db9b2e362c11333dac4052
nathaniel <nagoldfarb@wpi.edu>
2021-08-13 13:10:29 -0400

    maybe getting results

---
+++ double_pend/testing_system/modifed_model/play_all_cost_parameters.m
@@ -0,0 +1,65 @@
+
+params = load('/home/nathanielgoldfarb/pendulm_model/double_pend/testing_system/modifed_model/tests/all_cost/parameters.mat')
+
+% human parameters
+way_points = [ 0 , -pi, 0;  0 -0.5*pi,0 ]
+
+mh = [11.824, 3.848]; 
+me = [ 2.11, 1.28]  + mh;
+
+% other model params
+lh = [0.324, 0.3900]
+le = [0.324, 0.3900] 
+
+
+
+myQ = diag([.1 10 0.001 0.001]);
+myR = diag([0.001 0.001 0.00 0.00]);
+
+
+spring = [2000, 0; 0, 2000];
+dampener = [1000, 0; 0, 1000];
+
+% spring = [100, 0; 0, 100];
+% dampener = [100, 0; 0, 100];
+
+Dm1 = [496.569821348200,0;0,100];
+Km1 = [6017.73151361233,0;0,2898.16820260871];
+
+
+
+
+
+% model 1 gains
+% Km1 = [300, 0; 0 300];
+% Dm1 = [300,0;0,300];
+% 
+% 
+% Dm1 = [496.569821348200,0;0,100]
+% Km1 = [6017.73151361233,0;0,2898.16820260871]
+
+% Addmittance Controller
+Kd = [50,0;0, 50];
+Bd = [50,0;0, 50];
+Md = [10.0, 0; 0, 10.0];
+
+
+%Works well with the lower masses
+lambda2 = params.all_cost_parameters(9,1).Value;
+rho2 = params.all_cost_parameters(8,1).Value;
+exo_sat = 150;
+human_sat = 150;
+beta = params.all_cost_parameters(6,1).Value;
+
+
+Kem = [ 50, 0; 0, 50];
+Ked = [ 50, 0; 0, 50];
+
+
+Kp0 = params.all_cost_parameters(4,1).Value;
+Kn0 = params.all_cost_parameters(3,1).Value;
+Bp0 = params.all_cost_parameters(2,1).Value;
+Bn0 = params.all_cost_parameters(1,1).Value;
+alpha = params.all_cost_parameters(5,1).Value;
+gamma = params.all_cost_parameters(7,1).Value;
+
---
+++ double_pend/testing_system/modifed_model/plot_state_space.m
@@ -0,0 +1,60 @@
+
+linewidth = 2;
+
+x1_all = squeeze(all_cost_state_space.x(1,:,:)) % reshape magic
+xd1_all = squeeze(all_cost_state_space.xd(1,:,:)) % reshape magic
+
+
+x1_pos = squeeze(pos_cost_state_space.x(1,:,:)) % reshape magic
+xd1_pos = squeeze(pos_cost_state_space.xd(1,:,:)) % reshape magic
+
+
+x1_vel = squeeze(vel_cost_state_space.x(1,:,:)) % reshape magic
+xd1_vel = squeeze(vel_cost_state_space.xd(1,:,:)) % reshape magic
+
+
+x1_un = squeeze(untunned_state_space.x(1,:,:)) % reshape magic
+xd1_un = squeeze(untunned_state_space.xd(1,:,:)) % reshape magic
+
+
+
+x2_all = squeeze(all_cost_state_space.x(2,:,:)) % reshape magic
+xd2_all = squeeze(all_cost_state_space.xd(2,:,:)) % reshape magic
+
+
+x2_pos = squeeze(pos_cost_state_space.x(2,:,:)) % reshape magic
+xd2_pos = squeeze(pos_cost_state_space.xd(2,:,:)) % reshape magic
+
+
+x2_vel = squeeze(vel_cost_state_space.x(2,:,:)) % reshape magic
+xd2_vel = squeeze(vel_cost_state_space.xd(2,:,:)) % reshape magic
+
+
+
+x2_un = squeeze(untunned_state_space.x(1,:,:)) % reshape magic
+xd2_un = squeeze(untunned_state_space.xd(1,:,:)) % reshape magic
+
+
+subplot(2,1,1);
+hold on
+plot(x1_all, xd1_all,'LineWidth',linewidth);
+plot(x1_pos, xd1_pos,'LineWidth',linewidth);
+plot(x1_vel, xd1_vel,'LineWidth',linewidth);
+plot(x1_un, xd1_un,'LineWidth',linewidth);
+title('Joint 1')
+xlabel('$X$','interpreter','latex','FontSize',20)
+ylabel('$\dot{X}$','interpreter','latex', 'rotation',0, 'FontSize',20)
+legend('pos+vel cost','pos cost', 'vel cost', "untunned")
+
+
+subplot(2,1,2);
+hold on
+plot(x2_all, xd2_all,'LineWidth',linewidth);
+plot(x2_pos, xd2_pos,'LineWidth',linewidth);
+plot(x2_vel, xd2_vel,'LineWidth',linewidth);
+plot(x2_un, xd2_un,'LineWidth',linewidth);
+title('Joint 2')
+xlabel('$X$','interpreter','latex','FontSize',20)
+ylabel('$\dot{X}$','interpreter','latex', 'rotation',0, 'FontSize',20)
+legend('pos+vel cost','pos cost', 'vel cost', "untunned")
+
